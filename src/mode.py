2cc4f397e396c5c5ae888cb71901cf36eb132c90
chanchal <chanchal@gmail.com>
2026-09-09 22:46:05 +0530

    added login.html

--- src/mode.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Mode(Enum):
-    HUMAN = "This mode lets you play the game yourself."
-    GRAPH = "This mode lets an AI play the game based on graph algorithms."
-    RL = "This mode lets an AI play the game based on reinforcement learning."
